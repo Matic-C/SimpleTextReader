726e270d6f7b89346a52db70408baf013294c49c
unknown <matic.colnaric@kraft-werk.si>
2021-11-15 21:20:43 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.ComponentModel;
+
+namespace TxtReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly WordCounter _wordCounter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            progressBar.Value = 0;
+            _wordCounter = new WordCounter();
+            _wordCounter.worker.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
+            _wordCounter.worker.ProgressChanged += backgroundWorker_ProgressChanged;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Open file select window
+            Microsoft.Win32.OpenFileDialog openTxtFile = new Microsoft.Win32.OpenFileDialog()
+            {
+                InitialDirectory = @"C:\Users\%UserProfile%\Desktop",
+                Title = "Browse Text Files",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openTxtFile.ShowDialog() == true)
+            {
+                try
+                {
+                    if ((openTxtFile.OpenFile()) != null)
+                    {
+                        // Disable start button.
+                        start.IsEnabled = false;
+                        // Go
+                        _wordCounter.worker.RunWorkerAsync(openTxtFile.FileName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        // Update progress
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            percentage.Text = e.ProgressPercentage.ToString()+" %";
+        }
+
+        // Cancel the asynchronous operation.
+        private void Cancel_Click(System.Object sender, System.EventArgs e)
+        {
+            _wordCounter.worker.CancelAsync();
+        }
+
+        // Completed operation
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled != true)
+            {
+                List<Results> row = new List<Results>();
+
+                var sortedDictionary = (Dictionary<string, int>)e.Result;
+                
+                foreach (KeyValuePair<string, int> kvp in sortedDictionary.OrderByDescending(key => key.Value))
+                {
+                    row.Add(new Results() { Word = kvp.Key, Occurrence = kvp.Value });
+                }
+
+                wordResults.ItemsSource = row;
+                progressBar.Value = 100;
+                percentage.Text = "100 %";
+                MessageBox.Show("Done");
+            }
+            else
+            {
+                MessageBox.Show("Canceled");
+            }
+            
+            // Enble start button.
+            start.IsEnabled = true;
+        }
+
+        public class Results
+        {
+            public string Word { get; set; }
+            public int Occurrence { get; set; }
+        }
+
+    }
+}
---
+++ WordCounter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+
+namespace TxtReader
+{
+    class WordCounter
+    {
+        public readonly BackgroundWorker worker = new BackgroundWorker();
+
+        public WordCounter()
+        {
+            worker = new BackgroundWorker { WorkerReportsProgress = true };
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += ProcessTxt;
+        }
+
+        public void ProcessTxt(object sender, DoWorkEventArgs e)
+        {
+            // Get the BackgroundWorker
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            var file = e.Argument as string;
+            var filename = file; //Open file
+            var txtContent = File.ReadAllText(filename).Split(new[] { ' ', '\n', '\r'}, StringSplitOptions.RemoveEmptyEntries); //Split words
+            Dictionary<string, int> RepeatedWordCount = new Dictionary<string, int>();
+
+            var j = 0;
+            for (int i = 0; i < txtContent.Length; i++) //loop the splited string  
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    j++;
+                    if (RepeatedWordCount.ContainsKey(txtContent[i])) // Check if word already exists  
+                    {
+                        int value = RepeatedWordCount[txtContent[i]];
+                        RepeatedWordCount[txtContent[i]] = value + 1;
+                    }
+                    else
+                    {
+                        RepeatedWordCount.Add(txtContent[i], 1);  // Add repeated string 
+                    }
+
+                    // Avoid program freezing by decreasing ReportProgress operations
+                    if (j > 1000)
+                    {
+                        if (j % 1000 == 0)
+                        {
+                            float percent = ((float)(j + 1) / txtContent.Length) * 100;
+                            worker.ReportProgress((int)percent);
+                        }
+                    }
+                    else
+                    {
+                        float percent = ((float)(j + 1) / txtContent.Length) * 100;
+                        worker.ReportProgress((int)percent);
+                    }
+                }   
+            }
+            e.Result = RepeatedWordCount;
+        }
+    }
+}
